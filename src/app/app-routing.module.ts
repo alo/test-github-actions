f1f730d8f878a69d3ca9765bb68ab2b22e9bb9fe
Álvaro Quirós <alvaro.quiros@gmail.com>
2020-03-28 21:25:34 +0100

    wip

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
-
+const routes: Routes = [
+  {
+    path: 'test',
+    loadChildren: () => import('./test/test.module').then(m => m.TestModule)
+  },
+  {
+    path: '**',
+    redirectTo: '/'
+  }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
